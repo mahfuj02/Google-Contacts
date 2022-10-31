68450dca3bd011ccc5c62902272b2738edd3d337
Mahfuj Ahmed <ahmed.mahfuj02@gmail.com>
2022-11-01 02:52:17 +0600

    Project name rename

--- contacts_backend/asgi.py
+++ contacts_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for contactbackend project.
+ASGI config for contacts_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contactbackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contacts_backend.settings')
 
 application = get_asgi_application()
